de1124958fe3f60b366b530195a10389bfcf1df5
deadlybutter <thedeadlybutter@gmail.com>
2017-02-21 15:09:07 -0500

    Downloads reportback images

---
+++ README.md
@@ -0,0 +1,15 @@
+# reportback downloader
+
+### setup
+
+`npm install`
+
+### usage
+
+grab reportbacks
+
+`node app.js -campaign 1234 -run 5678 -count 20`
+
+clear reportbacks from folder
+
+`npm run clear`
--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const request = require('superagent');
 const argv = require('minimist')(process.argv.slice(2));
+
+const campaignId = argv.campaign || '3590';
+const campaignRunId = argv.run || '7636';
+const count = argv.count || 25;
+
+const request = require('superagent');
+const url = `https://www.dosomething.org/api/v1/reportbacks?campaigns=${campaignId}&runs=${campaignRunId}&status=approved,promoted&count=${count}`;
+
 const download = require('download-file');
+const downloadOptions = {
+  directory: './downloads/',
+  filename: 'default.png',
+};
+
+request(url)
+  .then(res => res.body.data)
+  .then(reportbacks => reportbacks.forEach((reportback, index) => {
+    downloadOptions.filename = `${campaignId}_${campaignRunId}-${index}.png`;
+    const mediaUri = reportback.reportback_items.data[0].media.uri;
+
+    download(mediaUri, downloadOptions, function(err) {
+      if (err) console.error(err);
+      console.info('downloaded', index);
+    })
+  }));
